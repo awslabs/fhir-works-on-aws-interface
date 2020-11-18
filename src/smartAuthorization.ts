dd868ee028f698f579f9f816558f80b11078cee4
Tim Nguyen <thingut@amazon.com>
2020-11-18 13:21:35 -0500

    Move comment to be above method

--- src/smartAuthorization.ts
+++ src/smartAuthorization.ts
@@ -2,11 +2,11 @@
  *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *  SPDX-License-Identifier: Apache-2.0
  */
-/**
- * Retrieve SMART scopes that are supported by the Auth server
- * http://hl7.org/fhir/smart-app-launch/scopes-and-launch-context/index.html#quick-start
- * @return a list of scopes supported by the Auth server
- */
 export interface SmartAuthorization {
+    /**
+     * Retrieve SMART scopes that are supported by the Auth server
+     * http://hl7.org/fhir/smart-app-launch/scopes-and-launch-context/index.html#quick-start
+     * @return a list of scopes supported by the Auth server
+     */
     getSupportedSMARTScopes(): Promise<string[]>;
 }
