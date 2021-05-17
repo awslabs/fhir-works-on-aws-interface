2e7cef5fe02e147db8e070fe0676068f1cdbea7d
Mike Lohmeier <mike.lohmeier@resmed.com>
2021-05-17 10:24:15 -0500

    [Issue #290] removed ttlInSeconds field because we now pass in ttlsInSeconds from deployment to persistence via the constructor

--- src/persistence.ts
+++ src/persistence.ts
@@ -9,7 +9,6 @@ export interface CreateResourceRequest {
     resourceType: string;
     resource: any;
     id?: string;
-    ttlInSeconds?: number; // archive timestamp in Unix epoch time format in seconds
 }
 
 export interface UpdateResourceRequest {
@@ -17,7 +16,6 @@ export interface UpdateResourceRequest {
     resourceType: string;
     resource: any;
     vid?: string; // used in version aware update
-    ttlInSeconds?: number; // archive timestamp in Unix epoch time format in seconds
 }
 
 export interface PatchResourceRequest {
@@ -25,7 +23,6 @@ export interface PatchResourceRequest {
     resourceType: string;
     resource: any;
     vid?: string; // used in version aware patch
-    ttlInSeconds?: number; // archive timestamp in Unix epoch time format in seconds
 }
 
 export interface ReadResourceRequest {
