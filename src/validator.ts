4cca5a76edd719b4040379a757e610f9373937c4
zheyanyu <zheyanyu@amazon.com>
2022-01-19 12:26:49 -0500

    Combine two optional params

--- src/validator.ts
+++ src/validator.ts
@@ -5,5 +5,5 @@ export interface Validator {
      * returns a resolved Promise if the resource is valid. Otherwise throws an error
      * @throws InvalidResourceError
      */
-    validate(resource: any, tenantId?: string, httpVerb?: VerbType): Promise<void>;
+    validate(resource: any, params: { enableMultiTenancy?: string; httpVerb?: VerbType }): Promise<void>;
 }
