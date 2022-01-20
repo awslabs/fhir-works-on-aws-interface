7fa981da34b05c2132debe3f5dc67badd44ab605
zheyanyu <zheyanyu@amazon.com>
2022-01-20 15:23:23 -0500

    destructuring param

--- src/validator.ts
+++ src/validator.ts
@@ -5,5 +5,8 @@ export interface Validator {
      * returns a resolved Promise if the resource is valid. Otherwise throws an error
      * @throws InvalidResourceError
      */
-    validate(resource: any, params?: { tenantId?: string; typeOperation?: TypeOperation }): Promise<void>;
+    validate(
+        resource: any,
+        { tenantId, typeOperation }: { tenantId?: string; typeOperation?: TypeOperation },
+    ): Promise<void>;
 }
