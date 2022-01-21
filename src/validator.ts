d65165ada3589a8dcfe3cd0b6411020b4bdf6965
Yanyu Zheng <yz2690@columbia.edu>
2022-01-21 14:28:04 -0500

    feat: Validator change (#97)

--- src/validator.ts
+++ src/validator.ts
@@ -1,7 +1,12 @@
+import { TypeOperation } from './constants';
+
 export interface Validator {
     /**
      * returns a resolved Promise if the resource is valid. Otherwise throws an error
      * @throws InvalidResourceError
      */
-    validate(resource: any, tenantId?: string): Promise<void>;
+    validate(
+        resource: any,
+        { tenantId, typeOperation }: { tenantId?: string; typeOperation?: TypeOperation },
+    ): Promise<void>;
 }
