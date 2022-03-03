a980196b6ff153efc816d8e35353118169757d68
Nestor Carvantes <carvantes@gmail.com>
2022-03-02 19:40:47 -0800

    feat!: add methods to support FHIR Subscriptions (#100)
    * feat: add getActiveSubscriptions interface (#95)
    * feat!: search and validate update for subscription data validation(#94)
    * feat: Validator change (#97)
    
    Co-authored-by: Sukeerth Vegaraju <ssvegaraju@yahoo.co.in>
    Co-authored-by: Yanyu Zheng <yz2690@columbia.edu>
    
    BREAKING CHANGE: add new required methods `getActiveSubscriptions` and `validateSubscriptionSearchCriteria`

--- src/stubs.ts
+++ src/stubs.ts
@@ -33,6 +33,10 @@ export module stubs {
         globalSearch(request) {
             throw new Error('Method not implemented.');
         },
+
+        validateSubscriptionSearchCriteria(searchCriteria) {
+            throw new Error('Method not implemented.');
+        },
     };
 
     export const history: History = {
@@ -256,6 +260,10 @@ export module stubs {
         },
 
         conditionalDeleteResource(request, queryParams) {
+            throw new Error('Method not implemented.');
+        },
+
+        getActiveSubscriptions(params: { tenantId: string }) {
             throw new Error('Method not implemented.');
         },
     };
